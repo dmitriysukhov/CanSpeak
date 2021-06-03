bb5039fb3c76134ae257e536c65e49ffc8a40cff
Сухов Дмитрий <Sukhov.D.Yur@sberbank.ru>
2021-06-03 14:30:45 +0300

    First commit

---
+++ src/test/java/com/dmitrysukhov/canspeak/CanspeakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitrysukhov.canspeak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanspeakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
